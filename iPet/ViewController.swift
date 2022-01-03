5839cf5cf1fd8fa9eb8b1abffe5242a047d3f7d3
zloysergunya <zloysergunya@mail.ru>
2022-01-03 14:20:46 +0700

    Initial Commit

---
+++ iPet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iPet
+//
+//  Created by Sergey Kotov on 03.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
