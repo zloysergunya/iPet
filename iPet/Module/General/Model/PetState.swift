2f1827c07995a299d18cc21726c23bf9e1e2791b
zloysergunya <zloysergunya@mail.ru>
2022-06-14 15:41:26 +0700

    + | ModalPetState | Add module

--- iPet/Module/General/Model/PetState.swift
+++ iPet/Module/General/Model/PetState.swift
@@ -7,13 +7,23 @@ enum PetState: Int {
     case chubby
     case swamFat
     
-    var description: String {
+    var name: String {
         switch self {
-        case .excellentBody: return "отличная форма"
-        case .gained: return "поднабрал"
-        case .inBody: return "в теле"
-        case .chubby: return "пухлый"
-        case .swamFat: return "заплыл"
+        case .excellentBody: return "Отличная форма"
+        case .gained: return "Поднабрал"
+        case .inBody: return "В теле"
+        case .chubby: return "Пухлый"
+        case .swamFat: return "Заплыл"
+        }
+    }
+    
+    var stateDescription: String {
+        switch self {
+        case .excellentBody: return "Всё по плану: ты выполняешь ежедневную норму шагов, а значит у зверька нет лишнего веса"
+        case .gained: return "Пора немного сбросить вес! Зверёк снова будет в отличной форме, если ты пройдёшь суммарно на 10% больше шагов в течение 7 дней"
+        case .inBody: return "Ты ещё можешь всё исправить! Надо в течение 7 дней суммарно увеличить норму шагов на 15%, и тогда зверёк вернётся в лучшую форму"
+        case .chubby: return "Не сдавайся! Если за 7 дней суммарно прибавишь норму шагов на 20%, твой любимец скажет тебе спасибо"
+        case .swamFat: return "Придётся попотеть! Ты забыл про своего зверька, и он подсел на сериалы и фастфуд. Суммарно увеличь норму шагов на 30% в течение 7 дней, и зверёк забудет про Netflix."
         }
     }
     
---
+++ iPet/Module/Modals/ModalPetState/View/ModalPetStateView.swift
@@ -0,0 +1,122 @@
+import UIKit
+import SnapKit
+
+class ModalPetStateView: RootView {
+    
+    private let containerView: UIView = {
+        let view = UIView()
+        view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        view.layer.cornerRadius = 10.0
+        
+        return view
+    }()
+    
+    private let thumbView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 2.0
+        view.backgroundColor = .init(hex: 0xC4C4C4)
+        
+        return view
+    }()
+    
+    let viewForClose: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        
+        return view
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .black
+        label.font = R.font.sfuiTextMedium(size: 18.0)
+        
+        return label
+    }()
+    
+    let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .black
+        label.font = R.font.sfuiTextRegular(size: 18.0)
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    let actionButton: UIButton = {
+        let button = UIButton()
+        button.titleLabel?.font = R.font.sfuiTextSemibold(size: 17.0)
+        button.setTitleColor(R.color.background(), for: .normal)
+        button.layer.cornerRadius = 10.0
+        
+        return button
+    }()
+    
+    let blurView: VisualEffectView = {
+        let blurView = VisualEffectView()
+        blurView.colorTint = .init(hex: 0xF4F4F4).withAlphaComponent(0.5)
+        blurView.blurRadius = 6.0
+        
+        return blurView
+    }()
+
+    override func setup() {
+        super.setup()
+        
+        containerView.backgroundColor = R.color.background()
+        
+        addSubview(blurView)
+        addSubview(containerView)
+        addSubview(viewForClose)
+        
+        containerView.addSubview(thumbView)
+        containerView.addSubview(titleLabel)
+        containerView.addSubview(subtitleLabel)
+        containerView.addSubview(actionButton)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        blurView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        containerView.snp.makeConstraints { make in
+            make.right.left.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+        
+        viewForClose.snp.makeConstraints { make in
+            make.bottom.equalTo(containerView.snp.top)
+            make.top.left.right.equalToSuperview()
+        }
+        
+        thumbView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(24.0)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(36.0)
+            make.height.equalTo(4.0)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(thumbView.snp.bottom).offset(16.0)
+            make.right.left.equalToSuperview().inset(24.0)
+        }
+        
+        subtitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(30.0)
+            make.left.right.equalToSuperview().inset(24.0)
+        }
+        
+        actionButton.snp.makeConstraints { make in
+            make.top.equalTo(subtitleLabel.snp.bottom).offset(30.0)
+            make.left.right.equalToSuperview().inset(64.0)
+            make.bottom.equalTo(safeAreaLayoutGuide.snp.bottom).offset(-30.0)
+            make.height.equalTo(56.0)
+        }
+    }
+    
+}
