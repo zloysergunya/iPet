eb9a0d26c879c3a1406bbe3421c9d6ba35d14ae8
zloysergunya <zloysergunya@mail.ru>
2022-06-25 23:40:06 +0700

    * | ModelError | Update error message

--- iPet/Common/Model/ModelError.swift
+++ iPet/Common/Model/ModelError.swift
@@ -9,6 +9,9 @@ struct ModelError: Error {
     var err: ErrorResponse?
     var text: String?
     
+    @Autowired
+    private var authService: AuthService?
+    
     init() {
         
     }
@@ -33,21 +36,19 @@ struct ModelError: Error {
             
             log.error("Error \(status): \(error.localizedDescription)")
             
-//            let message = "Error \(status): "
-//            switch status {
-//            case 401:
-//                let authService: AuthService? = ServiceLocator.getService()
-//                authService?.deauthorize()
-//                
-//                return message + "Ошибка авторизации. Пожалуйста, войдите в свой аккаунт заново"
-//                
-//            case 403: return message + "Доступ запрещён"
-//            case 404: return message + "Данные не найдены"
-//            case 400...499: return message + "Ошибка в запросе на сервер"
-//            case 500...599: return message + "Сервер временно недоступен. Попробуйте позднее"
-//            case 1000: return message + "Проверьте доступ к интернету"
-//            default: return message + "Неизвестная ошибка (не удалось декодировать ошибку)"
-//            }
+            let message = "Error \(status): "
+            switch status {
+            case 401:
+                authService?.deauthorize()                
+                return message + "Ошибка авторизации. Пожалуйста, войдите в свой аккаунт заново"
+                
+            case 403: return message + "Доступ запрещён"
+            case 404: return message + "Данные не найдены"
+            case 400...499: return message + "Ошибка в запросе на сервер"
+            case 500...599: return message + "Сервер временно недоступен. Попробуйте позднее"
+            case 1000: return message + "Проверьте доступ к интернету"
+            default: return message + "Неизвестная ошибка (не удалось декодировать ошибку)"
+            }
             
             return "Unknown error (failed to decode error)"
         }
